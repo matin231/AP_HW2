233ad8ca725050597a1e7e6dabd459d546d71e15
matin amj <amjadyan231@gmail.com>
2025-11-26 15:30:24 +0330

    Move java files inti Q1

---
+++ Q1/Person.java
@@ -0,0 +1,17 @@
+public abstract class Person {
+    private String name;
+    private String phoneNumber;
+
+    public Person(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public abstract String getInfo();
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+}
